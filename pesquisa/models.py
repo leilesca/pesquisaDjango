e3882bb9b761278ff216e3a60b48c23bfeca1513
Leila Mantovani <leila.yuriko@gmail.com>
2019-02-26 21:58:48 -0300

    Mexer na models

--- pesquisa/models.py
+++ pesquisa/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Pergunta(models.Model):
+    texto = models.CharField()
+    data = models.DateField()
+
+class Alternativa(models.Model):
+    texto = models.CharField()
+    votos =  models.IntegerField()
+
+
