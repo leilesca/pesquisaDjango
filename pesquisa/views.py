1f8acc3dcbf237fd4455f834772292714a76ba17
leila <leila.yuriko@gmail.com>
2019-02-27 21:49:53 -0300

    Editar index

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
+from .models import Pergunta
 
 # Create your views here.
 def index(request):
-    return render(request, 'index')
+    todas = Pergunta.objects.all()
+    return render(request, 'index.html', {
+        "perguntas": todas   
+        })
