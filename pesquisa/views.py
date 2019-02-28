1bebb52eea2215f922b7d7ff333467dd6f7c9a87
leila <leila.yuriko@gmail.com>
2019-02-28 20:56:10 -0300

    Mexer no html

--- pesquisa/views.py
+++ pesquisa/views.py
@@ -4,6 +4,14 @@ from .models import Pergunta
 # Create your views here.
 def index(request):
     todas = Pergunta.objects.all()
-    return render(request, 'index.html', {
-        "perguntas": todas   
+    contexto = {
+            'perguntas': todas
+    }
+    return render(request, 'index.html', contexto)
+
+def responder(request, num_pergunta):
+        pergunta = Pergunta.objects.get(pk=num_pergunta)
+        return render(request, 'responder',{
+                'numero': pergunta
         })
+
